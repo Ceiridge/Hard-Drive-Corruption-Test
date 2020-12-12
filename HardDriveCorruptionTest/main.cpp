d86bc7a07db1812a031aad2df0053202ba4d8e63
Ceiridge <philipp34533@icloud.com>
2020-12-12 23:03:18 +0100

    Fix: Fixed for large sizes

--- HardDriveCorruptionTest/main.cpp
+++ HardDriveCorruptionTest/main.cpp
@@ -3,9 +3,11 @@
 #include <string>
 #include <time.h>
 
+const size_t FiftyMB = 50 * 1000 * 1000;
+
 // All inclusive
-int RandomNumber(int min, int max) {
-	return (rand() % ((max + 1) - min)) + min;
+int64_t RandomNumber(int64_t min, int64_t max) {
+	return ((int64_t)rand() % ((max + 1) - min)) + min; // rand() only generates 32-bit of random data, so it's pseudo-pseudo-random
 }
 
 int main(int argc, char* argv[]) {
@@ -32,10 +34,10 @@ int main(int argc, char* argv[]) {
 			size_t bytesWritten = 0;
 
 			while (bytesWritten < fileSize) {
-				int nextSize = RandomNumber(1, std::min(50 * 1000 * 1000, (int)(fileSize - bytesWritten))); // Max 50 MB
+				size_t nextSize = RandomNumber(1, std::min(FiftyMB, fileSize - bytesWritten)); // Max 50 MB
 				char* buffer = new char[nextSize]; // Init buffer with given size
 
-				for (int i = 0; i < nextSize; i++) { // Fill buffer
+				for (size_t i = 0; i < nextSize; i++) { // Fill buffer
 					buffer[i] = (char)RandomNumber(0, CHAR_MAX);
 				}
 				file.write(buffer, nextSize);
@@ -62,11 +64,11 @@ int main(int argc, char* argv[]) {
 			size_t bytesRead = 0;
 
 			while (fileSize > bytesRead) { // Super code quality right here:
-				int nextSize = RandomNumber(1, std::min(50 * 1000 * 1000, (int)(fileSize - bytesRead))); // Max 50 MB
+				size_t nextSize = RandomNumber(1, std::min(FiftyMB, fileSize - bytesRead)); // Max 50 MB
 				char* readBuffer = new char[nextSize];
 
 				file.read(readBuffer, nextSize); // Fill read buffer
-				for (int i = 0; i < nextSize; i++) {
+				for (size_t i = 0; i < nextSize; i++) {
 					if (readBuffer[i] != (char)RandomNumber(0, CHAR_MAX)) { // Check if the bytes are equal to the random func.
 						std::cerr << "WARNING! BYTES MISMATCH AT " << (bytesRead + i) << "! YOUR DRIVE IS CORRUPT." << std::endl;
 						return 0;
