17be1a502084e238b904d13515f5b200ed2db939
Ceiridge <philipp34533@icloud.com>
2020-12-12 22:10:29 +0100

    Added: Basic functionality for reading file size

--- HardDriveCorruptionTest/main.cpp
+++ HardDriveCorruptionTest/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello World!\n";
+int main(int argc, char* argv[]) {
+	if (argc <= 1) {
+		std::cerr << "Usage: HardDriveCorruptionTest.exe <SizeInBytes>" << std::endl;
+		return 1;
+	}
+
+	try {
+		size_t fileSize = std::stoull(argv[1]);
+		std::cout << "Checking with " << fileSize << " bytes..." << std::endl;
+
+		return 0;
+	}
+	catch (std::exception& ex) {
+		std::cerr << "An exception occurred: " << ex.what() << std::endl;
+		return 1;
+	}
 }
